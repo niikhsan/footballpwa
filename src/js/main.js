69081575418c903f269c5ccb530caa1a16a88211
niikhsan <54101872+niikhsan@users.noreply.github.com>
2020-11-16 21:21:34 +0700

    Update main.js

--- src/js/main.js
+++ src/js/main.js
@@ -1,8 +1,8 @@
 import "../css/style.css";
 import "../../materialize/js/materialize.min.js";
-import "./sw-register.js";
-import loadNav from "./modules/nav.js";
-import loadPage from "./modules/page.js";
+import "/sw-register.js";
+import loadNav from "/modules/nav.js";
+import loadPage from "/modules/page.js";
 
 let path = window.location.hash.substr(1);
 path ? path = path : path = "home";
